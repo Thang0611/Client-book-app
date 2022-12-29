63610dac60003cb607a80c0a4f0425a2ad4ba964
Thang Nguyen <nguyenhuuthanga3@gmail.com>
2022-12-29 11:27:08 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Register from './Page/Register';
+import Login from './Page/Login';
+import Home from './Page/Home';
+import DetailBook from './Page/DetailBook';
+import AddBook from './Page/AddBook';
+import Order from './Page/Order';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path='' element={<Home />}></Route>
+      <Route path='login' element={<Login />}></Route>
+      <Route path='register' element={<Register />}></Route>
+      <Route path='detail' element={<DetailBook />}></Route>
+      <Route path='book' element={<AddBook />}></Route>
+      <Route path='order' element={<Order />}></Route>
+  </Routes>
+  )
+  
 }
 
 export default App;
